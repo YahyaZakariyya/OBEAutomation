b63f5c5a6acfd7963fd61372110ff45a29465215
Syed Abdullah Hassan Kazmi <iamabdullah234@gmail.com>
2024-09-12 11:53:25 +0500

    updatedCLO
    Issue for unique constraint in CLO w.r.t course is resolved

--- obesystem/models.py
+++ obesystem/models.py
@@ -130,11 +130,15 @@ class CourseLearningOutcome(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_outcomes')
     CLO = models.IntegerField(
         choices=[(i, str(i)) for i in range(1, 16)],  # Dropdown from 1 to 15
-        validators=[MinValueValidator(1)],
-        unique=True
+        validators=[MinValueValidator(1)]
     )
     description = models.TextField()
     mapped_to_PLO = models.ManyToManyField(ProgramLearningOutcome, related_name='related_clos')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['course', 'CLO'], name='unique_clo_per_course')
+        ]
 
     def __str__(self):
         return f"CLO {self.CLO}: {self.description}"
