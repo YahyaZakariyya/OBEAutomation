abf77a190917df88d18b8e15e8048a307b92e01c
Yahya <yahyabinzakariyya@gmail.com>
2024-11-03 11:05:13 +0500

    Added Jazzmin UI Tweaks

--- OBEAutomation/settings.py
+++ OBEAutomation/settings.py
@@ -43,11 +43,43 @@ INSTALLED_APPS = [
     'django_extensions',
 
     # custom apps
-    # 'obesystem.apps.ObesystemConfig',
     'obesystem.apps.ObesystemConfig',
 ]
 
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-navy",
+    "navbar": "navbar-gray-dark navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-navy",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
+
 JAZZMIN_SETTINGS = {
+    "show_ui_builder": True,
     "site_title": "OBE Automation",
     "site_header": "OBE Automation",
     "site_logo": "images/logo.png",
