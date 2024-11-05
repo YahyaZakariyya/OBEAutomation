badae1d6e2fa536dfe46d659e2ec68eb9d0d387d
Yahya Zakariyya <95631340+YahyaZakariyya@users.noreply.github.com>
2024-11-05 18:18:04 +0500

    Refactor models configuration (#11)
    * Added Fake User Entries
    
    * Refactor models configurations
    
    Refactor admin configurations: split models.py into modular files for better organization

---
+++ obesystem/models/__init__.py
@@ -0,0 +1,11 @@
+from .assessment import Assessment
+from .clo import CourseLearningOutcome
+from .course import Course
+from .custom_user import CustomUser
+from .mapping import ProgramCLOMapping
+from .plo import ProgramLearningOutcome
+from .program import Program
+from .question import Question
+from .section import Section
+
+__all__ = ['Assessment', 'CourseLearningOutcome', 'Course', 'CustomUser', 'ProgramCLOMapping', 'ProgramLearningOutcome', 'Program', 'Question', 'Section']
