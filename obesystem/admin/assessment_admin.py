0554c40a1571f358b994098541a80543c3ae418c
Yahya <yahyabinzakariyya@gmail.com>
2024-12-11 02:25:27 +0500

    Implement role-based dashboards in Django admin
    This commit includes the following:
    - Overridden the Django admin index.html to dynamically include role-based dashboards.
    - Created separate dashboard templates for Admin, Faculty, and Student roles.
    - Ensured the admin_dashboard.html is styled properly and included centralized styles in base_site.html.
    - Adjusted template paths and TEMPLATES configuration for proper template rendering.
    - Verified dynamic inclusion of dashboards based on user roles (admin, faculty, student) using CustomUser model's role field.

--- obesystem/admin/assessment_admin.py
+++ obesystem/admin/assessment_admin.py
@@ -7,14 +7,25 @@ from django.utils.html import format_html
 
 class AssessmentAdmin(GuardedModelAdmin):
     inlines = [QuestionInline]
-
+    
     list_display = ['title', 'section', 'date', 'type', 'weightage', 'manage_marks_button']
     fields = ['title', 'section', 'date', 'type', 'weightage']
     
+    def changelist_view(self, request, extra_context=None):
+        # Pass the request to a custom attribute
+        self.current_request = request
+        return super().changelist_view(request, extra_context)
+    
     def manage_marks_button(self, obj):
-        return format_html('<a href="/edit-scores/?id={}" target="_blank">Manage Marks</a>', obj.id)
+        if obj:  # Ensure obj is not None
+            request = getattr(self, 'current_request', None)
+            if request and self.has_change_permission(request, obj):
+                return format_html('<a class="btn btn-sm btn-primary" href="/edit-scores/?id={}" target="_blank">Manage Marks</a>', obj.id)
+            else:
+                return format_html('<a href="/view-scores/?id={}" target="_blank">View Marks</a>', obj.id)
+        return "-"  # Return a default placeholder if obj is None
 
-    manage_marks_button.short_description = 'Manage Marks'  # Column header in admin
+    manage_marks_button.short_description = 'Manage/View Marks'
     manage_marks_button.allow_tags = True
         
     def get_form(self, request, obj=None, **kwargs):
@@ -32,7 +43,14 @@ class AssessmentAdmin(GuardedModelAdmin):
                 # Faculty members can only see sections they have `view_section` permission for
                 allowed_sections = get_objects_for_user(request.user, 'obesystem.view_section', klass=Section)
                 form.base_fields['section'].queryset = allowed_sections
-
+        
+        section_id = request.GET.get('section_id__exact')  # Retrieve section_id from GET
+        if section_id and not obj:  # Adding a new object from filtered view
+            form.base_fields['section'].initial = section_id  # Pre-fill section
+            form.base_fields['section'].disabled = True  # Lock section field
+            form.base_fields.pop('section', None)
+        elif obj:  # Editing an existing object
+            form.base_fields['section'].disabled = True  # Lock section field
         return form
 
     def get_queryset(self, request):
@@ -48,18 +66,17 @@ class AssessmentAdmin(GuardedModelAdmin):
 
     def has_module_permission(self, request):
         return True
-    
+
     def has_view_permission(self, request, obj=None):
         if request.user.is_superuser:
             return True
         if obj:
             return request.user.has_perm('obesystem.view_assessment', obj)
         return True
-    
+
     def has_add_permission(self, request, obj=None):
         """
-        Allow faculty to add Assessment only if they have the `can_add_assessment` permission
-        for the related Section.
+        Allow faculty to add Assessment only if they have the `can_add_assessment` permission for the related Section.
         """
         if obj is None:  # For the changelist or adding a new object
             # Ensure the user has add permission for at least one section
@@ -78,7 +95,7 @@ class AssessmentAdmin(GuardedModelAdmin):
             return True
         # Check if the user has the required permission for the related section
         return request.user.has_perm('obesystem.change_assessment', obj)
-    
+
     def has_delete_permission(self, request, obj=None):
         """
         Allow faculty to delete Assessments if they have delete permission
@@ -87,5 +104,5 @@ class AssessmentAdmin(GuardedModelAdmin):
         if obj is None:  # For the changelist view
             return True
         return request.user.has_perm('obesystem.delete_assessment', obj)
-    
+
 admin.site.register(Assessment, AssessmentAdmin)
